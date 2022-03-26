4fd332123a7f81d0961e7d8b830c19da1ca58921
Cyanne Jones <jonescyanne@yahoo.com>
2022-03-26 13:10:10 -0600

    Add completed meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,60 @@
-function nameMenuItem() {
+//global variables
+var deliciousNameGlobal;
+var menuItemGlobal;
+var ingredients = [];
+var formattedPriceGlobal;
+var recipe = {};
 
-}
+function nameMenuItem(normalName) {
 
+  deliciousNameGlobal = `Delicious ${normalName}`
+  return deliciousNameGlobal;
+};
+
+
+function createMenuItem(menuNameParam, menuPriceParam, menuTypeParam) {
+  menuItemGlobal = {
+    name: menuNameParam,
+    price: menuPriceParam,
+    type: menuTypeParam
+  }
+  return menuItemGlobal
+};
+
+
+function addIngredients(ingredientToAdd, ingredientArray) {
+  if (ingredientArray.includes(ingredientToAdd) === false) {
+     ingredients = ingredientArray.push(ingredientToAdd);
+     return ingredients;
+  }
+};
+
+
+function formatPrice(initialPriceParam) {
+  formattedPriceGlobal = `$${initialPriceParam}`
+  return formattedPriceGlobal
+};
+
+function decreasePrice(decreasedPriceParam){
+  return decreasedPriceParam * 0.9
+};
+
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    ingredients: ingredients,
+    title: deliciousNameGlobal,
+    price:formattedPriceGlobal,
+    type: menuItemType
+  }
+  return recipe
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
